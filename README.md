ca6efcef9a36eca14a4d2d5d6fc1f042a04f05b2
lxw0109 <lxw0109@gmail.com>
2017-11-01 19:58:51 +0800

    track by git

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
 # Leetcode
 
 Our Hacking practice on [Leetcode](https://leetcode.com/problemset/algorithms/).
-
-### Problem Set
-
-| **Number** | **Title** | **Solution** | **Type** | **Difficulty** |
-| --- | ------ | -------- | ---------- | -------- |
-| 461 | [Hamming Distance](https://leetcode.com/problems/hamming-distance/description/) | [num461](https://github.com/lxw0109/LeetCode_lxw/blob/master/LeetCode_Python/num001/num001.py) | 	Bit Manipulation | Easy |
-
-
-. 
---
+++ lxw/num461/num461.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# FileName: num461.py
+# Author: lxw
+# Date: 11/1/17 7:55 PM
+
+"""
+Num 461: Hamming Distance
+Source: https://leetcode.com/problems/hamming-distance/description/
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different..
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
