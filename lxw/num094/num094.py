a7b5dddb78656ce256addf1782adcdc7aa8ff669
lxw0109 <lxw0109@gmail.com>
2017-11-19 13:04:41 +0800

    add num094.py. TODO: the time complexity of recursive algorithms.

--- lxw/num094/num094.py
+++ lxw/num094/num094.py
@@ -21,13 +21,17 @@ return [1,3,2].
 Note: Recursive solution is trivial, could you do it iteratively?
 """
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+'''
+# Recursive
 class Solution:
     def _in_order_traveral(self, root, result):
         if not root:
@@ -38,27 +42,43 @@ class Solution:
 
     def inorderTraversal(self, root):
         """
-        Time:
+        Time:  # TODO:
         :type root: TreeNode
         :rtype: List[int]
         """
         result = []
         self._in_order_traveral(root, result)
         return result
+'''
 
-
-
-"""
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+# Iterative
+# Method 2:
 class Solution:
     def inorderTraversal(self, root):
         """
+        Time: 52ms. Great.
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result, stack = [], []
+        while 1:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                break
+            root = stack.pop()
+            result.append(root.val)
+            root = root.right
+        return result
+
+
+'''
+# Method 1:
+class Solution:
+    def inorderTraversal(self, root):
+        """
+        Time: 62ms. Tedious and Unreadable.
         :type root: TreeNode
         :rtype: List[int]
         """
@@ -68,21 +88,41 @@ class Solution:
             return result
         node = root
         while 1:
-                        
-            while node.left:                
+            # 1. 循环遍历目标节点target_node的左斜线节点
+            while node.left:
                 stack.append(node)
                 node = node.left
+
+            # 2. node的left为空，node.val入result
             result.append(node.val)
+
+            # 3. 寻找下一个目标节点target_node
             if node.right:
                 node = node.right
-            while len(stack) > 0:
-                node = stack.pop()
-                result.append(node.val)
-                if node.right:
-                    node = node.right
-                    break
-                
+            else:
+                if stack:
+                    find = False
+                    while stack:
+                        node = stack.pop()
+                        result.append(node.val)
+                        if node.right:
+                            node = node.right
+                            find = True
+                            break
+                    if not find:
+                        break
+                else:
+                    break    # The Exit.
         return result
-        
-"""
-        
+'''
+
+
+if __name__ == '__main__':
+    node1 = TreeNode(1)
+    node2 = TreeNode(2)
+    node3 = TreeNode(3)
+    node1.right = node2
+    node2.left = node3
+    sol = Solution()
+    print(sol.inorderTraversal(node1))
+
